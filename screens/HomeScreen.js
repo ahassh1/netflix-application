ce3d6abad8c0240b44df37bcd59b46d0e5be685f
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-14 04:20:27 +0600

    SafeAreaView and StatusBar added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,13 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, SafeAreaView, StatusBar } from 'react-native';
 import React from 'react';
 
 const HomeScreen = () => {
   return (
-    <View className="relative flex-1">
-      <Image
-        className="absolute h-full w-full"
-        source={require('../assets/avenger.jpg')}
-        resizeMode="cover"
-      />
-      <Text className="h-24 w-32 bg-red-600 pt-4 text-center font-bold text-white">HomeScreen</Text>
-      <Text className="mt-2 text-white">Polash</Text>
+    <View className="flex-1 bg-neutral-800">
+      {/* searchbar and logo */}
+      <SafeAreaView className="-mb-2">
+        <StatusBar style="light" />
+      </SafeAreaView>
     </View>
   );
 };
