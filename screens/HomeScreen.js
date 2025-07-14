b60759427ca9473112f0dddf8c815b5a00e34a94
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-14 19:51:59 +0600

    icon added and title theme colored

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,15 @@
-import { View, Text, Image, SafeAreaView, StatusBar, Platform } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
-import { Bars3CenterLeftIcon } from 'react-native-heroicons/outline';
+import { Bars3CenterLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import { style } from 'theme';
 const ios = Platform.OS === 'ios';
 
 const HomeScreen = () => {
@@ -11,6 +20,14 @@ const HomeScreen = () => {
         <StatusBar style="light" />
         <View className="mx-4 flex-row items-center justify-between">
           <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+          <Text
+            className="text-3xl font-bold
+           text-white">
+            <Text style={style.text}>Net</Text>Flim
+          </Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon size="30" color="white" strokeWidth={2} />
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     </View>
---
+++ theme/index.js
@@ -0,0 +1,8 @@
+export const theme = {
+  text: '#eab308',
+  background: '#eab308',
+};
+export const style = {
+  text: { color: theme.text },
+  background: { backgroundColor: theme.background },
+};
